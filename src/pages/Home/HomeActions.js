057c73fc119bab35cce05dfd857c85823536e03e
Jake Irwin <Jake.Irwin@ibm.com>
2021-10-16 18:14:55 -0400

    Fix endpoint

--- src/pages/Home/HomeActions.js
+++ src/pages/Home/HomeActions.js
@@ -36,7 +36,7 @@ export const getBooks = () => {
 export const getAudiobooks = () => {
   const callBackendAPI = async () => {
     const response = await fetch(
-      "http://audimend-env.eba-kanxbpya.us-east-1.elasticbeanstalk.com/similarity"
+      "http://audimend-env.eba-kanxbpya.us-east-1.elasticbeanstalk.com/audiobooks"
     );
     const body = await response.json();
 
